d0af238c94a5d6f4616908c1f08f46c494e92775
woldu <eyerusalemdani@gmail.com>
2021-10-14 11:04:06 -0700

    HW1

---
+++ app/src/main/java/com/example/androidad_340/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.androidad_340
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        title="My Android App"
+
+
+    }
+}
